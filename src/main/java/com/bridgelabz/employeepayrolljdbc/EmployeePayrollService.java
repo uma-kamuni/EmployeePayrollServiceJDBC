9e307fb45f1b81fcfca0d26db23e04a84953e7ab
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-25 00:57:19 -0800

    Welcome to Employee Payroll Service JDBC Program

---
+++ src/main/java/com/bridgelabz/employeepayrolljdbc/EmployeePayrollService.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.employeepayrolljdbc;
+
+public class EmployeePayrollService {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Employee Payroll Service JDBC program");
+
+	}
+
+}
